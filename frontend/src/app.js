b003dfd896d742492c311c7e089502539b0360cb
Kevin Lin <kevinptt0323@gmail.com>
2017-01-08 18:55:28 +0800

    add react

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,1 +1,16 @@
-console.log("XD");
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        Hello app!
+      </div>
+    );
+  }
+}
+
+export default App;
