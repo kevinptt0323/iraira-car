ed8c7b3d354b4565fdc7ca5f75e751d060884299
Kevin Lin <kevinptt0323@gmail.com>
2017-01-09 14:22:29 +0800

    finish basic intersect detection

--- frontend/src/Components/gameboard.js
+++ frontend/src/Components/gameboard.js
@@ -1,7 +1,7 @@
 import React, { PropTypes }  from 'react';
-import { Stage, Layer, Rect, Line } from 'react-konva';
+import { Stage, Layer, Group, Rect, Line } from 'react-konva';
 
-import { Block, Goal } from './objects'
+import { Block, Goal, VirtualCursor } from './objects'
 
 const config = {
   bgcolor: "#000"
@@ -10,9 +10,24 @@ const config = {
 class Gameboard extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      cursorX: 50,
+      cursorY: 125,
+      startX: 50,
+      startY: 125,
+      blocks: [
+        [0, 0, 800, 0, 800, 300, 300, 300, 300, 100, 0, 100],
+        [0, 600, 800, 600, 800, 350, 250, 350, 250, 150, 0, 150]
+      ],
+      goals: [
+        [750, 300, 800, 300, 800, 350, 750, 350]
+      ]
+    };
 
     this.handleBlock = this.handleBlock.bind(this);
     this.handleGoal = this.handleGoal.bind(this);
+    this.handleLockChange = this.handleLockChange.bind(this);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
   }
   handleBlock() {
     const { increaseScore } = this.context;
@@ -21,6 +36,41 @@ class Gameboard extends React.Component {
   handleGoal() {
     const { increaseScore } = this.context;
     increaseScore(100);
+  }
+  handleMouseMove(e) {
+    const { width, height } = this.props;
+    let { cursorX, cursorY, startX, startY } = this.state;
+    cursorX += e.movementX;
+    cursorY += e.movementY;
+    if (cursorX<0) cursorX = 0;
+    if (cursorX>=width) cursorX = width-1;
+    if (cursorY<0) cursorY = 0;
+    if (cursorY>=height) cursorY = height-1;
+    const curr = {x: cursorX, y: cursorY};
+    if (this.refs.blocks.getIntersection(curr)) {
+      [cursorX, cursorY] = [startX, startY];
+      this.handleBlock();
+    } else if (this.refs.goals.getIntersection(curr)) {
+      [cursorX, cursorY] = [startX, startY];
+      this.handleGoal();
+    }
+    this.setState({cursorX, cursorY});
+  }
+  handleLockChange() {
+    const canvas = this.refs.mainCanvas.getLayer().getCanvas()._canvas;
+    if (document.pointerLockElement === canvas) {
+      document.addEventListener("mousemove", this.handleMouseMove, false);
+    } else {
+      document.removeEventListener("mousemove", this.handleMouseMove, false);
+    }
+  }
+  componentDidMount() {
+    const canvas = this.refs.mainCanvas.getLayer().getCanvas()._canvas;
+    canvas.addEventListener('click', (e) => {e.target.requestPointerLock();}, false);
+    document.addEventListener('pointerlockchange', this.handleLockChange, false);
+  }
+  componentWillUnmount() {
+    document.exitPointerLock();
   }
   render() {
     const {
@@ -36,10 +86,18 @@ class Gameboard extends React.Component {
             width={width} height={height}
             fill={config.bgcolor} />
         </Layer>
-        <Layer>
-          <Block points={[0, 0, 800, 0, 800, 300, 300, 300, 300, 100, 0, 100]} onmouseover={this.handleBlock} />
-          <Block points={[0, 600, 800, 600, 800, 350, 250, 350, 250, 150, 0, 150]} onmouseover={this.handleBlock} />
-          <Goal points={[750, 300, 800, 300, 800, 350, 750, 350]} onmouseover={this.handleGoal} />
+        <Layer ref="blocks">
+          {this.state.blocks.map((points, idx) =>
+            <Block key={`block-${idx}`} points={points} />
+          )}
+        </Layer>
+        <Layer ref="goals">
+          {this.state.goals.map((points, idx) =>
+            <Goal key={`goal-${idx}`} points={points} />
+          )}
+        </Layer>
+        <Layer ref="mainCanvas">
+          <VirtualCursor x={this.state.cursorX} y={this.state.cursorY} radius={5} />
         </Layer>
       </Stage>
     );
--- frontend/src/Components/objects.js
+++ frontend/src/Components/objects.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Line } from 'react-konva';
+import { Line, Circle } from 'react-konva';
 
 export const Block = ({points, onmouseover}) => (
   <Line
@@ -18,5 +18,15 @@ export const Goal = ({points, onmouseover}) => (
     strokeWidth={0}
     closed={true}
     onmouseover={onmouseover}
+    />
+);
+
+export const VirtualCursor = ({x, y, radius=1}) => (
+  <Circle
+    x={x}
+    y={y}
+    radius={radius}
+    fill="blue"
+    strokeWidth={0}
     />
 );
